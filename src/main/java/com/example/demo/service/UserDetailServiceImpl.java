f8fe9dadebe233078f865e9d3a29109571fa75c8
Z7-SP7S2 <568143444@qq.com>
2021-01-19 21:16:27 +0800

    throw

--- src/main/java/com/example/demo/service/UserDetailServiceImpl.java
+++ src/main/java/com/example/demo/service/UserDetailServiceImpl.java
@@ -1,5 +1,9 @@
 package com.example.demo.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.demo.entity.Users;
+import com.example.demo.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
@@ -9,15 +13,25 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 @Service("userDetailsService")
-public class UserDetailServiceImpl  implements UserDetailsService {
+public class UserDetailServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        List<GrantedAuthority> list= AuthorityUtils.commaSeparatedStringToAuthorityList("admin");
-        return new User("marry",new BCryptPasswordEncoder().encode("456"),list);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        QueryWrapper<Users> wrapper = new QueryWrapper<>();
+        wrapper.eq("username", username);
+        Users users = userMapper.selectOne(wrapper);
+        if (users == null) {
+            throw new UsernameNotFoundException("用户名不存在");
+        } else {
+            List<GrantedAuthority> list = AuthorityUtils.commaSeparatedStringToAuthorityList("admin");
+            return new User(users.getUsername(), new BCryptPasswordEncoder().encode(users.getPassword()), list);
+        }
+
     }
 }
